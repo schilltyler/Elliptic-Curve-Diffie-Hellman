611acaf83fc3b38e041e02609683e8eace238933
Tyler Schill <tylerjschill@gmail.com>
2026-06-01 10:55:20 -0400

    started changing math around

--- ecdh.c
+++ ecdh.c
@@ -15,19 +15,6 @@
  *
  * secp256r1 (aka P-256)
  *
- * p: 0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff
- * (115792089210356248762697446949407573530086143415290314195533631308867097853951)
- * a: 0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc
- * (115792089210356248762697446949407573530086143415290314195533631308867097853948)
- * b: 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
- * (41058363725152142129326129780047268409114441015993725554835256314039467401291)
- * Gx: 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
- * (48439561293906451759052585252797914202762949526041747995844080717082404635286)
- * Gy: 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
- * (36134250956749795798585127919587881956611106672985015071877198253568414405109)
- * n: 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
- * (115792089210356248762697446949407573529996955224135760342422259061068512044369)
- * h: 1
  */
 #define p "115792089210356248762697446949407573530086143415290314195533631308867097853951"
 #define a "115792089210356248762697446949407573530086143415290314195533631308867097853948"
@@ -40,13 +27,6 @@
 /*
  * Test values:
  *
- * p: 23
- * a: 1
- * b: 1
- * Gx: 4
- * Gy: 5
- * n: 7
- * h: 1
  */ 
 
 #define test_p 23
@@ -56,6 +36,60 @@
 #define test_Gy 5
 #define test_n 7
 #define test_h 1
+
+/*
+ * Point Structure
+ */
+typedef struct point {
+    
+    BIGNUM *x; // x-coordinate
+    BIGNUM *y; // y-coordinate
+
+} point_t;
+
+/*
+ * Hashed Key Derivation Function
+ *
+ * Definition: produces a 128 bit key
+ *
+ * Parameters:
+ *
+ * salt:
+ *
+ * ikm: (input key material) this is the shared secret
+ * we generate from Diffie Hellman
+ *
+ */
+long hkdf(long salt, ikm) {
+
+    long key;
+
+    return key;
+}
+
+point_t* point_addition(point_t *p, point_t *q) {
+
+    point_t *result = malloc(sizeof(point_t));
+
+    if (result == NULL) {
+        fprintf(stderr, "Could not malloc new point\n");
+        return NULL;
+    }
+
+    BIGNUM *y_slope = BN_new();
+
+
+
+    //(q->y - p->y) / (q->x - p->x);
+
+    // (slope)^2 - x_two - x_one
+    int x_sum = 0;
+
+    // slope(x_two - x_one) - y_one
+    int y_sum = 0;
+
+    return result;
+}
 
 int main() {
 
